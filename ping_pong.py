11103d51b74195296fa1fdb7ef06527a570da2d5
prostosmoke <example_2008@inbox.ru>
2022-12-03 15:38:08 +0300

    В игру добавлен мячик и его движение

--- ping_pong.py
+++ ping_pong.py
@@ -7,8 +7,14 @@ FPS = 60
 
 window = display.set_mode((W_W , W_H))
 display.set_caption('Пинг-Понг')
-window.fill((0, 255, 239))
 
+
+ball = image.load('ball.png')
+ball = transform.scale(ball, (30, 30))
+ball_x = 50
+ball_y = 50
+ball_speed_x = 3
+ball_speed_y = 3
 
 clock = time.Clock()
 
@@ -23,5 +29,21 @@ while not game_over:
         if e.type == QUIT:
             game_over = True
 
+    
+    #правила
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
+    if ball_y+30 >= W_H:
+        ball_speed_y *= -1
+    if ball_x+30 >= W_W:
+        ball_speed_x *= -1
+    if ball_y < 0:
+        ball_speed_y *= -1
+    if ball_x < 0:
+        ball_speed_x *= -1
 
+        
+    #отрисовка
+    window.fill((0, 255, 239))
+    window.blit(ball, (ball_x, ball_y))
     display.update()
