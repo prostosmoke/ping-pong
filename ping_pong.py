3b93f5dc0a99d78674a2bbb2892c29d1c081cdb3
prostosmoke <example_2008@inbox.ru>
2022-12-17 15:05:10 +0300

    Добавлены игроки, классы

--- ping_pong.py
+++ ping_pong.py
@@ -4,46 +4,68 @@ from pygame import *
 W_W = 700
 W_H = 500
 FPS = 60
+BG_COLOR = (0, 255, 239)
 
 window = display.set_mode((W_W , W_H))
 display.set_caption('Пинг-Понг')
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, x, y, w, h, speed, image_name):
+        self.img = image.load(image_name)
+        self.img = transform.scale(self.img, (w, h))
+        self.rect = Rect(x, y, w ,h)
+        self.speed_x = speed
+        self.speed_y = speed
 
-ball = image.load('ball.png')
-ball = transform.scale(ball, (30, 30))
-ball_x = 50
-ball_y = 50
-ball_speed_x = 3
-ball_speed_y = 3
+    def update(self):
+        window.blit(self.img, (self.rect.x, self.rect.y))
+
+class Ball(GameSprite):
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.y+30 >= W_H:
+            self.speed_y *= -1
+        if self.rect.x+30 >= W_W:
+            self.speed_x *= -1
+        if self.rect.y < 0:
+            self.speed_y *= -1
+        if self.rect.x < 0:
+            self.speed_x *= -1
+        super().update()
+
+class Player(GameSprite):
+    def __init__(self, x, y, w, h, speed, image_name, k_up, k_down):
+        super().__init__(x, y, w, h, speed, image_name)
+        self.k_up = k_up
+        self.k_down = k_down
+
+    def update(self):
+        key_pressed = key.get_pressed()
+        if key_pressed[self.k_up]:
+            self.rect.y -= self.speed_y
+        elif key_pressed[self.k_down]:
+            self.rect.y += self.speed_y
+        super().update()
+
+ball = Ball(50, 50, 30, 30, 3, 'ball.png')
+player1 = Player(10, 200, 20, 100, 5, 'player.png', K_w, K_s)
+player2 = Player(670, 200, 20, 100, 5, 'player.png', K_UP, K_DOWN)
 
 clock = time.Clock()
 
-
-game_over = False
-is_gameplay = True
-while not game_over:
+game = True
+while game:
     clock.tick(FPS)
 
 
     for e in event.get():
         if e.type == QUIT:
-            game_over = True
-
-    
-    #правила
-    ball_x += ball_speed_x
-    ball_y += ball_speed_y
-    if ball_y+30 >= W_H:
-        ball_speed_y *= -1
-    if ball_x+30 >= W_W:
-        ball_speed_x *= -1
-    if ball_y < 0:
-        ball_speed_y *= -1
-    if ball_x < 0:
-        ball_speed_x *= -1
-
-        
+            game = False
+            
     #отрисовка
-    window.fill((0, 255, 239))
-    window.blit(ball, (ball_x, ball_y))
+    window.fill(BG_COLOR)
+    ball.update()
+    player1.update()
+    player2.update()
     display.update()
